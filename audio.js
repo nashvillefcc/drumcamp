cac1da80398f01dfd4ed9b2bce762d491cb06de8
Pete Hurl <mellowmilieu@gmail.com>
2019-06-19 18:56:34 -0500

    Minor stylistic edits and updated JS audio functionality for repeated clicks

--- audio.js
+++ audio.js
@@ -1,24 +1,12 @@
-$(function () {
-    $(".playback").click(function (e) {
-        e.preventDefault();
+const display = document.getElementById("display");
 
-        // This next line will get the audio element
-        // adjacent to button clicked.
-        var clip = $(this).next('audio').get(0);
-        if (clip.paused)
-            clip.play();
-        else
-            clip.pause();
-    });
+$(".playback").click(function (e) {
+    e.preventDefault();
+
+    var clip = $(this).next('audio').get(0);
+
+    display.innerHTML = clip.id;
+
+    clip.currentTime = 0;
+    clip.play();
 });
-//
-// courtesy of @jessicadavey (github) / @jessicadavey (Discord) Thank you Jessica!
-//
-const buttons = [...document.querySelectorAll("buttons")];
-buttons.map(button => button.addEventListener("click", function (e) {
-
-    const nameOfClip = ($(this).next('audio').get(0).id);
-    const display = document.getElementById("display");
-    display.innerHTML = nameOfClip;
-
-}));
